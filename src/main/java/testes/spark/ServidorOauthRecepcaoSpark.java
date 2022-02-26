2036acebac72fa4773ece6fde324a0cbb1ae862c
Salvio Furbino <salviof@gmail.com>
2022-02-26 02:30:58 -0300

    Atualizacao automática

--- src/main/java/testes/spark/ServidorOauthRecepcaoSpark.java
+++ src/main/java/testes/spark/ServidorOauthRecepcaoSpark.java
@@ -5,10 +5,12 @@
  */
 package testes.spark;
 
+import com.super_bits.modulosSB.SBCore.ConfigGeral.SBCore;
 import com.super_bits.modulosSB.SBCore.integracao.libRestClient.api.token.ItfTokenGestaoOauth;
 import com.super_bits.modulosSB.SBCore.integracao.libRestClient.implementacao.TipoClienteOauth;
 import com.super_bits.modulosSB.SBCore.integracao.libRestClient.implementacao.UtilSBApiRestClient;
 import com.super_bits.modulosSB.SBCore.modulos.objetos.MapaObjetosProjetoAtual;
+import org.coletivojava.fw.api.tratamentoErros.FabErro;
 import spark.Spark;
 
 /**
@@ -32,23 +34,26 @@ public class ServidorOauthRecepcaoSpark extends Thread {
 
     @Override
     public void run() {
+        try {
+            Spark.port(Integer.valueOf(porta));
 
-        Spark.port(Integer.valueOf(porta));
+            Spark.get(caminho, (req, res) -> {
+                try {
+                    String tipoAplicacao = req.queryParamOrDefault("tipoAplicacao", null);
+                    if (UtilSBApiRestClient.receberCodigoSolicitacaoOauth(req.raw(), tipoAplicacao)) {
+                        return "Obrigado, agora estamos conectados com sua conta.";
+                    } else {
+                        return "Falha Gerando Código de acesso";
+                    }
 
-        Spark.get(caminho, (req, res) -> {
-            try {
-                String tipoAplicacao = req.queryParamOrDefault("tipoAplicacao", null);
-                if (UtilSBApiRestClient.receberCodigoSolicitacaoOauth(req.raw(), tipoAplicacao)) {
-                    return "Obrigado, agora estamos conectados com sua conta.";
-                } else {
-                    return "Falha Gerando Código de acesso";
+                } catch (Throwable t) {
+                    return "Erro Maluco" + t.getMessage();
                 }
 
-            } catch (Throwable t) {
-                return "Erro Maluco" + t.getMessage();
-            }
-
-        });
+            });
+        } catch (Throwable t) {
+            SBCore.RelatarErro(FabErro.SOLICITAR_REPARO, "Falha iniciando servidor na prta" + porta + " path:" + caminho + t.getMessage(), t);
+        }
 
     }
 
